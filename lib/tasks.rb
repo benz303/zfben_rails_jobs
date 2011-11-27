77ec9ca2fca026d9375819198d7e51ca73749aff
ben <ben@zfben.(none)>
2011-11-27 16:08:03 +0800

    v0.0.1

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,37 +1,37 @@
 namespace :jobs do
-  ROOT = File.realpath(Rails.root) << '/tmp/jobs/'
+  jobs_root = File.realpath(Rails.root) << '/tmp/jobs/'
   
   desc 'Init Jobs Folders'
   task :init do
-    FileUtils.mkdir(ROOT) unless File.exists? ROOT
+    FileUtils.mkdir(jobs_root) unless File.exists? jobs_root
   end
   
   desc 'Start Jobs'
   task :start => :init do
-    if File.exists? ROOT + '/.lock'
+    if File.exists? jobs_root + '/.lock'
       STDERR.print "Jobs are running, please run after they finished.\n"
       exit!
     else
-      File.open(ROOT + '/.lock', 'w'){ |f| f.write Process.pid.to_s }
+      File.open(jobs_root + '/.lock', 'w'){ |f| f.write Process.pid.to_s }
       print "Starting jobs at process##{Process.pid}\n"
-      list = Dir[ROOT + '*']
+      list = Dir[jobs_root + '*']
       if list.length > 0
         Rake::Task[:environment].execute
         list.each do |id|
-          jobs = Job.new
+          jobs = Jobs.new
           jobs.import File.basename(id)
           jobs.run
         end
       end
-      File.delete(ROOT + '/.lock')
+      File.delete(jobs_root + '/.lock')
       print "Finished jobs\n"
     end
   end
   
   desc 'Stop Jobs'
   task :stop do
-    if File.exists? ROOT + '/.lock'
-      system 'kill `cat ' + ROOT + '/.lock`'
+    if File.exists? jobs_root + '/.lock'
+      system 'kill `cat ' + jobs_root + '/.lock`'
     end
   end
 end
